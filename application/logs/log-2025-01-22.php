c087824044265c6ad6b27f4235566d22acdf92d3
Patrick Deba <10patrick.deba@gmail.com>
2025-01-31 10:18:07 +0100

    eliminacion de logs y autogeneracion de contraseña

--- application/logs/log-2025-01-22.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2025-01-22 09:49:02 --> Severity: 8192  --> Optional parameter $level declared before required parameter $msg is implicitly treated as a required parameter C:\xampp\htdocs\intranet_prueba\system\libraries\Log.php 72
